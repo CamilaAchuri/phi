504d665c6bc777d49f10241fe1b24999b19d36bf
Camila Achury <camilaachury.4@gmail.com>
2020-07-27 15:52:52 -0500

    add scrolling

---
+++ docs/static/js/phi.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+ var controller = new ScrollMagic.Controller();
+ var ourScene = new ScrollMagic.Scene({
+     triggerElement: '#banner-four'
+ })
+ .setClassToggle('#banner-four', 'fade-in')
+ .addIndicators()
+ .addTo(controller);
+
+
+
+});
+
+
+$(function () { // wait for document ready
+    // init
+    var controller = new ScrollMagic.Controller({
+        globalSceneOptions: {
+            triggerHook: 'onLeave'
+           }
+    });
+
+    // get all slides
+    var slides = document.querySelectorAll("section.select-animate");
+
+    // create scene for every slide
+    for (var i=0; i<slides.length; i++) {
+        new ScrollMagic.Scene({
+                triggerElement: slides[i]
+            })
+            .setPin(slides[i])
+            .addTo(controller);
+    }
+});
