156f1277c0f3d9b29c073c18aaee9b0690e84a79
Camila Achury <camilaachury.4@gmail.com>
2020-07-27 18:02:56 -0500

    scroll

--- docs/static/js/phi.js
+++ docs/static/js/phi.js
@@ -13,22 +13,64 @@ $(document).ready(function(){
 
 $(function () { // wait for document ready
     // init
-    var controller = new ScrollMagic.Controller({
-        globalSceneOptions: {
-            triggerHook: 'onLeave',
-            duration: 50
-           }
-    });
+    // var controller = new ScrollMagic.Controller({
+    //     globalSceneOptions: {
+    //         triggerHook: 'onLeave'
+    //        }
+    // });
 
-    // get all slides
-    var slides = document.querySelectorAll("section.select-animate");
+    // // get all slides
+    // var slides = document.querySelectorAll("section.select-animate");
 
-    // create scene for every slide
-    for (var i=0; i<slides.length; i++) {
+    // console.log(Math.floor(slides.innerHeight * 0.75));
+    // // create scene for every slide
+    // for (var i=0; i<slides.length; i++) {
+    //     new ScrollMagic.Scene({
+    //             triggerElement: slides[i]
+    //         })
+    //         .setPin(slides[i])
+    //         .addTo(controller);
+    // }
+
+    let controller;
+    let items = document.querySelectorAll("section");
+
+		items.forEach(function(element, index) {
+			let height = element.clientHeight; //height of current element
+            console.log(height);
+            var controller = new ScrollMagic.Controller({
+                globalSceneOptions: {
+				duration: height,
+				triggerHook: 'onLeave'
+            }
+        });
         new ScrollMagic.Scene({
-                triggerElement: slides[i]
-            })
-            .setPin(slides[i])
-            .addTo(controller);
-    }
+            triggerElement: element
+        })
+        .setPin(element)
+        .addTo(controller);
 });
+});
+
+
+// $(function () { 
+//     let controller;
+//     let items = document.querySelectorAll("section");
+
+// 		items.forEach(function(element, index) {
+// 			let height = element.clientHeight; //height of current element
+//             console.log(height);
+//             var controller = new ScrollMagic.Controller({
+//                 globalSceneOptions: {
+// 				duration: height,
+// 				triggerHook: 'onLeave'
+//             }
+//         });
+//         new ScrollMagic.Scene({
+//             triggerElement: element
+//         })
+//         .setPin(element)
+//         .addTo(controller);
+ 
+    
+//     });
