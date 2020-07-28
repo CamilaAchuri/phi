923eae05d0278cd6deaf29365a58929c1ac2caf3
Camila Achury <camilaachury.4@gmail.com>
2020-07-28 05:43:48 -0500

    .

--- docs/static/js/phi.js
+++ docs/static/js/phi.js
@@ -1,76 +1,37 @@
-$(document).ready(function(){
- var controller = new ScrollMagic.Controller();
- var ourScene = new ScrollMagic.Scene({
-     triggerElement: '#banner-four'
- })
- .setClassToggle('#banner-four', 'fade-in')
- .addTo(controller);
+$(function() {
+	let controller;
+	let items = document.querySelectorAll('section');
 
-
-
-});
-
-
-$(function () { // wait for document ready
-    // init
-    // var controller = new ScrollMagic.Controller({
-    //     globalSceneOptions: {
-    //         triggerHook: 'onLeave'
-    //        }
-    // });
-
-    // // get all slides
-    // var slides = document.querySelectorAll("section.select-animate");
-
-    // console.log(Math.floor(slides.innerHeight * 0.75));
-    // // create scene for every slide
-    // for (var i=0; i<slides.length; i++) {
-    //     new ScrollMagic.Scene({
-    //             triggerElement: slides[i]
-    //         })
-    //         .setPin(slides[i])
-    //         .addTo(controller);
-    // }
-
-    let controller;
-    let items = document.querySelectorAll("section");
-
-		items.forEach(function(element, index) {
-			let height = element.clientHeight; //height of current element
-            console.log(height);
-            var controller = new ScrollMagic.Controller({
-                globalSceneOptions: {
+	items.forEach(function(element, index) {
+		let height = element.clientHeight; //height of current element
+		console.log(height);
+		var controller = new ScrollMagic.Controller({
+			globalSceneOptions: {
 				duration: height,
-				triggerHook: 'onLeave'
-            }
-        });
-        new ScrollMagic.Scene({
-            triggerElement: element
-        })
-        .setPin(element)
-        .addTo(controller);
+                triggerHook: 'onLeave'
+			}
+		});
+		new ScrollMagic.Scene({
+			triggerElement: element
+		})
+			.setPin(element)
+			.addTo(controller);
+	});
 });
+
+$(function() {
+	var controller = new ScrollMagic.Controller();
+	var scene = new ScrollMagic.Scene({ triggerElement: '#trigger1', duration: 250}).setPin('#pin1').addTo(controller);
 });
 
+$(function() {
+	var controller = new ScrollMagic.Controller();
+	var scene = new ScrollMagic.Scene({ triggerElement: '#pin2', duration: 200}).setPin('#pin2').addTo(controller);
+});
 
-// $(function () { 
-//     let controller;
-//     let items = document.querySelectorAll("section");
+// $(document).ready(function(){
 
-// 		items.forEach(function(element, index) {
-// 			let height = element.clientHeight; //height of current element
-//             console.log(height);
-//             var controller = new ScrollMagic.Controller({
-//                 globalSceneOptions: {
-// 				duration: height,
-// 				triggerHook: 'onLeave'
-//             }
-//         });
-//         new ScrollMagic.Scene({
-//             triggerElement: element
-//         })
-//         .setPin(element)
-//         .addTo(controller);
- 
-    
-//     });
+// });
+
+
+	
